256bbbd228b91b6b7fff8fa01aca34f845efc5b1
Nicolas & Jhuly <nicolasdshoffmann2105@gmail.com>
2026-06-12 21:26:15 -0300

    Pratica 02

---
+++ pratica-03.js
@@ -0,0 +1,9 @@
+//valor da compra
+let valor = 110;
+
+//verifica se o valor é > ou = a 100
+if (valor >= 100) {
+    console.log('Cliente ganhou desconto!');  
+} else {
+    console.log('Cliente não ganhou desconto.')
+}
