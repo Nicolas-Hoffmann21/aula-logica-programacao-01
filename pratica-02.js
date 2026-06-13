ec3beab0b4c9f9d43e3bbddd40063a203d5ec718
Nicolas & Jhuly <nicolasdshoffmann2105@gmail.com>
2026-06-12 21:17:01 -0300

    Pratica 02

---
+++ pratica-02.js
@@ -0,0 +1,9 @@
+// Armazena o número inteiro a ser testado
+let numero = 15;
+
+// Verifica se o resto da divisão por 2 é igual a zero
+if (numero % 2 === 0) {
+    console.log(`O número ${numero} é PAR!`);
+} else {
+    console.log(`O número ${numero} é ÍMPAR!`);
+}
