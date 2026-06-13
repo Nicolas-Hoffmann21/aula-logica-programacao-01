7c5ddfe9b64b77a737a5ad69b4f63ca87cf5a57b
Nicolas & Jhuly <nicolasdshoffmann2105@gmail.com>
2026-06-12 21:13:35 -0300

    Pratica 01

---
+++ pratica-01.js
@@ -0,0 +1,9 @@
+// Armazena a nota final do aluno
+let notaFinal = 7.5; 
+
+// Verifica se a nota é maior ou igual a 6
+if (notaFinal >= 6) {
+    console.log('Aluno aprovado!');
+} else {
+    console.log('Aluno reprovado!');
+}
